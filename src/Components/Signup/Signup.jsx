a99ac73d5f586b01491b80e876119df5c2c38562
rahulsd380 <rahulsd380@gmail.com>
2024-02-04 22:09:55 +0600

    signup pages added

---
+++ src/Components/Signup/Signup.jsx
@@ -0,0 +1,187 @@
+import { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
+import Navbar from "../Navbar/Navbar";
+
+const Signup = () => {
+//   const axiosUser = useAxiosClient();
+//   const {updateProfileInfo, signUp, googleSignUp} = useContext(AuthContext);
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   const handleSignUp = e => {
+//       e.preventDefault();
+//       const name = e.target.name.value;
+//       const email = e.target.email.value;
+//       const password = e.target.password.value;
+//       console.log( email, name, password);
+
+//       const toastId = toast.loading("Signing up...")
+
+//       signUp(email, password)
+//     .then((result) => {
+//       console.log(result.user);
+//       updateProfileInfo(name)
+//       .then(() => {
+//         const userInfo = { name, email };
+//         axiosUser.post("/users", userInfo)
+//         .then((res) => {
+//           console.log(res.data);
+
+//           if (res.data.insertedId) {
+//             toast.success("Signed up successfully.", { id: toastId });
+//             navigate(location?.state ? location.state : "/");
+//           }
+//         });
+//       });
+//     })
+//     .catch((error) => console.log(error))
+
+//     .catch((error) => {
+//       console.log(error);
+//     });
+//   }
+
+
+//   // google signup
+//   const googleSignIn = () => {
+//       googleSignUp()
+//       .then(result => {
+//           console.log(result.user);
+//           navigate(location ?.state ? location.state : '/');
+//       })
+//       .catch(error => {
+//           console.error(error);
+//       })
+//   }
+
+// onSubmit={handleSignUp}
+// onClick={googleSignIn}
+
+    return (
+      <div>
+         <Navbar></Navbar>
+       <div className="max-w-6xl mx-auto py-10">
+     <div className="grid grid-cols-2 gap-10 items-center bg-white rounded-md shadow-md border border-gray-300">
+
+       {/* Left side form */}
+       <form className="p-14" >
+       <div>
+       <div className="flex justify-center mb-3">
+       <div className="flex items-center gap-3">
+          <img
+            className="w-10"
+            src="https://i.ibb.co/9ZDVRzh/logo.png"
+            alt=""
+          />
+          <h1 className="text-xl md:text-2xl font-bold bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
+            Strom Scope
+          </h1>
+        </div>
+       </div>
+         <h1 className="text-xl font-bold text-gray-600 text-center mb-2">
+           Welcome to <span className="bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">Strom Scope.</span> Get weather update in your location!
+         </h1>
+         
+
+         <div>
+           
+           
+
+           <div className="mb-2">
+             <p className="mb-1 font-semibold text-gray-600">Your Name</p>
+             <input
+             name="name"
+               className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
+               type="text"
+               placeholder="Rahul Sutradhar"
+             />
+           </div>
+
+           <div className="mb-2">
+             <p className="mb-1 font-semibold text-gray-600">Your Email</p>
+             <input
+             name="email"
+               className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
+               type="email"
+               placeholder="rahul@gmail.com"
+             />
+           </div>
+
+           <div className="mb-3">
+             <p className="mb-1 font-semibold text-gray-600">Password</p>
+             <input
+             name="password"
+               className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
+               type="password"
+               placeholder="*********"
+             />
+           </div>
+
+
+           <div className="mb-3 flex justify-between items-center">
+              <p className="textgray-600 font-semibold flex items-center gap-2"><input type="checkbox"  className="checkbox text-blue-600" /> Remember me</p>
+
+
+              <p className="textgray-600 font-semibold text-blue-400">Forgot Password?</p>
+           </div>
+
+           <button className="w-full font-semibold transition duration-300 bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-2 rounded text-white mb-3 hover:bg-gradient-to-r hover:from-blue-500 hover:to-cyan-500">
+             Sign Up
+           </button>
+
+           <p className="mb-3 text-center text-gray-600 font-semibold">Or,</p>
+
+
+           <button className="w-full font-semibold transition duration-300 border border-gray-300 hover:shadow-md px-4 py-2 rounded text-gray-600 mb-3 flex items-center gap-2 justify-center">
+           <FcGoogle></FcGoogle> Coninue with Google
+           </button>
+
+           <p className="mb-4 text-center">
+           Already Have An Account?{" "}
+           <Link to={"/login"} className="text-blue-500 font-semibold underline">Sign in</Link>
+         </p>
+         </div>
+       </div>
+       
+       </form>
+
+      {/* Right side banner */}
+       <div className="h-full">
+         <div
+           className="hero h-full"
+           
+         >
+            <video autoPlay loop muted className="w-full h-full object-cover">
+      <source src="/public/bg.mp4" type="video/mp4"/>
+    </video>
+           <div className="hero-overlay bg-opacity-60"></div>
+           <div className="hero-content text-center text-neutral-content">
+             <div className="max-w-md">
+               <h1 className="mb-5 text-4xl font-bold">
+               Sign Up for StormScope: Your Gateway to Accurate Weather Insights!
+               </h1>
+               <p className="mb-5">
+               Unlock precise weather forecasts with StormScope. Sign up now for real-time insights and stay ahead of the storm!
+               </p>
+               <button className="p-2 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md">Get Started</button>
+             </div>
+           </div>
+         </div>
+       </div>
+
+
+
+     </div>
+   </div>
+   <Toaster
+ position="bottom-center"
+ reverseOrder={false}
+/>
+   </div>
+    );
+};
+
+export default Signup;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Components/Root/Root";
 import Home from "./Components/Home/Home";
 import AuthProvider from "./Components/AuthProvider/AuthProvider";
+import Signup from "./Components/Signup/Signup";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
       },
     ],
   },
