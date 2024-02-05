0421321b17b5b928392a0b5d860e3a9a7a91a657
rahulsd380 <rahulsd380@gmail.com>
2024-02-05 14:22:57 +0600

    update or add info modal added

--- src/Components/AllUser/AllUserTable.jsx
+++ src/Components/AllUser/AllUserTable.jsx
@@ -1,13 +1,13 @@
 import toast, { Toaster } from "react-hot-toast";
-import { FaPersonRunning } from "react-icons/fa6";
-import { IoMdDoneAll } from "react-icons/io";
-import { IoPersonAddSharp } from "react-icons/io5";
-import { MdCancelPresentation, MdDelete, MdModeEditOutline } from "react-icons/md";
+import { MdDelete, MdModeEditOutline } from "react-icons/md";
 import { MdAddCard } from "react-icons/md";
 import { MdOutlineAirplanemodeInactive } from "react-icons/md";
 import useAxiosClient from "../../hooks/useAxiosClient";
 import useAllUser from "../../hooks/useAllUser";
 import { MdAirplanemodeActive } from "react-icons/md";
+import Swal from "sweetalert2";
+import { FaUserCircle } from "react-icons/fa";
+import { FaUserEdit } from "react-icons/fa";
 
 
 const AllUserTable = ({ user, index }) => {
@@ -47,20 +47,55 @@ const AllUserTable = ({ user, index }) => {
           }
         });
       };
+
+
+    //   Delete user functionality
+    const handleDeleteUser = (item) => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            axiosUser.delete(`/users/${item._id}`).then((res) => {
+              console.log(res);
+              if (res.data.deletedCount > 0) {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "User removed successfully.",
+                  icon: "success",
+                });
+                refetch();
+              }
+            });
+          }
+        });
+      };
+
+
     return (
             <tr className="bg-gradient-to-r from-slate-100 to-emerald-100 border-b-1 pb-2 border-gray-300">
       <td>{index + 1}</td>
-      <td>{name}</td>
+      <td className="flex items-center gap-2">
+      <FaUserCircle className="text-4xl text-blue-500"></FaUserCircle>
+      <div className="avatar online">
+  <div className="w-10 rounded-full">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  </div>
+</div>
+        {name}
+        </td>
       <td>{email}</td>
       <td>{addedDate}</td>
       <td>{status}</td>
       <td>
         <div className="flex items-center gap-2">
-        {/* onClick={() => {
-              handleDeleteTask(task);
-            }} */}
           <button
-            
+            onClick={() => {handleDeleteUser(user)}}
             className="p-2 border rounded-md bg-gray-50 flex justify-center items-center tooltip text-rose-600"
             data-tip="Delete Info"
           >
@@ -99,73 +134,77 @@ const AllUserTable = ({ user, index }) => {
                 document.getElementById(`my_modal_${_id}`).showModal()
               }
               className="p-2 border rounded-md bg-gray-50 flex justify-center items-center text-sky-600 tooltip"
-              data-tip="Update Info"
+              data-tip="Update or Add Info"
             >
-              <MdModeEditOutline></MdModeEditOutline>
+              <FaUserEdit></FaUserEdit>
             </button>
             <dialog id={`my_modal_${_id}`} className="modal">
               <div className="modal-box bg-gray-200">
                 <h3 className="font-bold text-lg text-gray-600 mb-6">
-                  Update Task Details
+                  Add more information about yourself
                 </h3>
                 {/* onSubmit={(e) => handleUpdateTask(e, _id)} */}
                 <form >
-                  <div className="grid grid-cols-1 gap-5">
-                    <div className="mb-2">
-                      <p className="mb-1 font-semibold text-gray-600">Title</p>
+                  <div className="grid grid-cols-1 gap-3">
+                    <div>
+                      <p className="mb-1 font-semibold text-gray-600">Country</p>
                       <input
                         required
-                        name="title"
+                        name="country"
                         className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
                         type="text"
-                        placeholder="Title"
+                        placeholder="Your country"
                       />
                     </div>
 
-                    <div className="mb-2">
+                    <div>
                       <p className="mb-1 font-semibold text-gray-600">
-                        Task Deadline
-                      </p>
-                      <input
-                        required
-                        name="deadline"
-                        className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
-                        type="date"
-                        placeholder="Enter the deadline"
-                      />
-                    </div>
-
-                    <div className="mb-2">
-                      <p className="mb-1 font-semibold text-gray-600">
-                        Priority Level
+                        Gender
                       </p>
                       <select
-                        name="priority"
+                      required
+                        name="gender"
                         className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
                       >
                         <option disabled selected>
                           Select
                         </option>
-                        <option value="low">Low</option>
-                        <option value="moderate">Moderate</option>
-                        <option value="high">High</option>
-                        <option value="most important">Most Important</option>
+                        <option value="low">Male</option>
+                        <option value="moderate">Female</option>
+                        <option value="moderate">Prefer not to say</option>
                       </select>
                     </div>
 
-                    <div className="mb-2">
-                      <p className="mb-1 font-semibold text-gray-600">
-                        Task Description
-                      </p>
-                      <textarea
-                        placeholder="Enter the task description"
+
+                    <div>
+                      <p className="mb-1 font-semibold text-gray-600">Date of Birth</p>
+                      <input
+                        required
+                        name="dob"
                         className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
-                        name="taskDescription"
-                        id=""
-                        cols="30"
-                        rows="10"
-                      ></textarea>
+                        type="date"
+                      />
                     </div>
+
+                    <div>
+                      <p className="mb-1 font-semibold text-gray-600">Phone Number</p>
+                      <input
+                        required
+                        name="phone"
+                        className="bg-white border border-gray-400 outline-none px-2 py-1 rounded w-full"
+                        type="number"
+                        placeholder="+880 11324334"
+                      />
+                    </div>
+
+                    <div>
+                      <p className="mb-1 font-semibold text-gray-600">Profile Picture</p>
+                      <input required type="file" className="file-input file-input-bordered file-input-info w-full max-w-xs" />
+                    </div>
+
+                    
+
+                    
                   </div>
 
                   <div className="flex flex-row-reverse items-center gap-9">
@@ -195,16 +234,6 @@ const AllUserTable = ({ user, index }) => {
               </div>
             </dialog>
           </div>
-          {/* onClick={() => {
-              handleMakeCompleted(task);
-            }} */}
-          <button
-           
-            className="p-2 border rounded-md bg-gray-50 flex justify-center items-center text-green-600 tooltip"
-            data-tip="Add Extra Info"
-          >
-            <MdAddCard></MdAddCard>
-          </button>
         </div>
       </td>
       <Toaster position="bottom-center" reverseOrder={false} />
