1dccaf8701575812605b6b24e87f9bb692319c66
rahulsd380 <rahulsd380@gmail.com>
2024-02-05 14:36:09 +0600

    filter by status functionality implemented

--- src/Components/AllUser/AllUser.jsx
+++ src/Components/AllUser/AllUser.jsx
@@ -1,21 +1,42 @@
 import Lottie from "lottie-react";
 import { Toaster } from "react-hot-toast";
-import { FaTasks } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 import { Link } from "react-router-dom";
 import useAllUser from "../../hooks/useAllUser";
 import finding from "../../../public/finding.json"
 import AllUserTable from "./AllUserTable";
+import { FaUsers } from "react-icons/fa6";
+import { useState } from "react";
 
 
 const AllUser = () => {
     const [allUser, isLoading, ] = useAllUser();
+
+    const [sortByActive, setSortByActive] = useState(false);
+    const [sortByInactive, setSortByInactive] = useState(false);
+
+  const handleSortByActive = () => {
+    setSortByActive(true);
+    setSortByInactive(false);
+  };
+
+  const handleSortByInactive = () => {
+    setSortByInactive(true); 
+    setSortByActive(false);
+  };
+      // Filter task
+  const filteredUser = allUser.filter((user) => {
+    const active = sortByActive ? user.status === "active" : true;
+    const inactive = sortByInactive ? user.status.toLowerCase() === "inactive" : true;
+
+    return active && inactive;
+  });
     return (
         <div className="max-w-7xl mx-auto pb-10">
       <div className="flex gap-3 md:gap-0 flex-wrap items-center justify-between">
         <div>
         <h1 className="text-2xl text-gray-500 font-bold flex items-center gap-2">
-          <FaTasks className="text-blue-400"></FaTasks> Manage All Tasks
+          <FaUsers className="text-blue-400"></FaUsers> Manage User Info
         </h1>
         </div>
 
@@ -23,39 +44,35 @@ const AllUser = () => {
           <div className="flex items-center gap-2">
             <p className="text-gray-600 font-semibold">Status Filter:</p>
             <details className="dropdown">
-              <summary className="bg-white border border-gray-300 hover:cursor-pointer hover:bg-teal-500 hover:text-white transition duration-300 p-2 rounded-md flex items-center gap-3">
+              <summary className="bg-white border border-gray-300 hover:cursor-pointer hover:bg-blue-500 hover:text-white transition duration-300 p-2 rounded-md flex items-center gap-3">
                 Filter <IoIosArrowDown></IoIosArrowDown>
               </summary>
-              {/* <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                 <li>
-                  <a onClick={() => handleSortByCompleted("Completed")}>
-                    Completed
+                
+                  <a onClick={() => handleSortByActive("active")}>
+                    Active
                   </a>
                 </li>
                 <li>
-                  <a onClick={() => handleSortByTodo("to-do")}>To-do</a>
+                  <a onClick={() => handleSortByInactive("inactive")}>Inactive</a>
                 </li>
-                <li>
-                  <a onClick={() => handleSortByOnGoing("On-going")}>
-                    On-going
-                  </a>
-                </li>
-              </ul> */}
+              </ul>
             </details>
           </div>
 
           <div className="flex items-center gap-2">
-            <p className="text-gray-600 font-semibold">Due Date Filter:</p>
-            {/* <input
+            <p className="text-gray-600 font-semibold">Filter By Added Date:</p>
+            <input
               type="date"
-              onChange={(e) => handleDueDateFilter(e.target.value)}
-              value={dueDateFilter || ""}
+            //   onChange={(e) => handleDueDateFilter(e.target.value)}
+            //   value={dueDateFilter || ""}
               className="border border-gray-300 px-2 py-1 rounded"
-            /> */}
+            />
           </div>
           <Link to={"/dashboard/addTask"}>
             <button className="text-white font-semibold px-4 py-2 bg-gradient-to-r from-cyan-500 to-blue-500  transition duration-300 rounded-md text-center">
-              Add New Task
+              Add New User
             </button>
           </Link>
           
@@ -84,7 +101,7 @@ const AllUser = () => {
             </div>
           ) : (
             <tbody>
-              {allUser.map((user, index) => (
+              {filteredUser.map((user, index) => (
                 <AllUserTable
                   key={user._id}
                   user={user}
